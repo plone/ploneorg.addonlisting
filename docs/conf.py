2797b1ebb2951b5fda31997bcec7621659ade547
Alexander Loechel <Alexander.Loechel@lmu.de>
2017-06-01 11:32:41 +0200

    apply review changes

--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,7 @@ master_doc = 'index'
 # General information about the project.
 project = u'ploneorg.addonlisting'
 copyright = u'2017, Plone Foundation and Contributors'
-author = u'Alexander Loechel'
+author = u'Plone Community'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -221,7 +221,7 @@ html_theme = 'alabaster'
 #html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'RestrictedPythondoc'
+htmlhelp_basename = 'ploneorg_addonlisting_doc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -243,8 +243,11 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'RestrictedPython.tex', u'RestrictedPython Documentation',
-     u'Alexander Loechel', 'manual'),
+    (master_doc,
+     'plone_addonlisting.tex',
+     u'Ploneorg Addonlisting Documentation',
+     u'Alexander Loechel',
+     'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -273,8 +276,10 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'restrictedpython', u'RestrictedPython Documentation',
-     [author], 1)
+    (master_doc, 'ploneorg_addonlisting',
+     u'PloneOrg Addonlisting Documentation',
+     [author],
+     1)
 ]
 
 # If true, show URL addresses after external links.
@@ -287,8 +292,12 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'ploneorg.addonlisting', u'ploneorg.addonlisting Documentation',
-     author, 'ploneorg.addonlisting', 'One line description of project.',
+    (master_doc,
+     'ploneorg.addonlisting',
+     u'ploneorg.addonlisting Documentation',
+     author,
+     'ploneorg.addonlisting',
+     'A Plugin that make Add-Ons accessible via a Plone instance.',
      'Miscellaneous'),
 ]
 
--- src/ploneorg/addonlisting/vocabularies.py
+++ src/ploneorg/addonlisting/vocabularies.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 """static and dynamic vocabularies."""
 
+# flake8: NOQA: E501
+
 from ploneorg.addonlisting import _
 from zope.interface import provider
 from zope.schema.interfaces import IVocabularyFactory
@@ -14,8 +16,7 @@ def framework_versions_dynamic_vocabulary(context):
     if context:
         versions = context.framework_version_mapping
         for version in versions:
-            terms.append(SimpleVocabulary.createTerm(version['key'],
-                                                     version['value']))
+            terms.append(SimpleVocabulary.createTerm(version['key'], version['value']))
     return SimpleVocabulary(terms)
 
 
@@ -25,8 +26,7 @@ def python_versions_dynamic_vocabulary(context):
     if context:
         versions = context.python_version_mapping
         for version in versions:
-            terms.append(SimpleVocabulary.createTerm(version['key'],
-                                                     version['value']))
+            terms.append(SimpleVocabulary.createTerm(version['key'], version['value']))
     return SimpleVocabulary(terms)
 
 
@@ -43,32 +43,21 @@ def addon_type_vocabulary(context):
 @provider(IVocabularyFactory)
 def addon_pypi_state_vocabulary(context):
     return SimpleVocabulary([
-        SimpleTerm(value=u'status-1',
-                   title=u'Development Status :: 1 - Planning'),
-        SimpleTerm(value=u'status-2',
-                   title=u'Development Status :: 2 - Pre-Alpha'),
-        SimpleTerm(value=u'status-3',
-                   title=u'Development Status :: 3 - Alpha'),
-        SimpleTerm(value=u'status-4',
-                   title=u'Development Status :: 4 - Beta'),
-        SimpleTerm(value=u'status-5',
-                   title=u'Development Status :: 5 - Production/Stable'),
-        SimpleTerm(value=u'status-6',
-                   title=u'Development Status :: 6 - Mature'),
-        SimpleTerm(value=u'status-7',
-                   title=u'Development Status :: 7 - Inactive'),
+        SimpleTerm(value=u'status-1', title=u'Development Status :: 1 - Planning'),
+        SimpleTerm(value=u'status-2', title=u'Development Status :: 2 - Pre-Alpha'),
+        SimpleTerm(value=u'status-3', title=u'Development Status :: 3 - Alpha'),
+        SimpleTerm(value=u'status-4', title=u'Development Status :: 4 - Beta'),
+        SimpleTerm(value=u'status-5', title=u'Development Status :: 5 - Production/Stable'),
+        SimpleTerm(value=u'status-6', title=u'Development Status :: 6 - Mature'),
+        SimpleTerm(value=u'status-7', title=u'Development Status :: 7 - Inactive'),
     ])
 
 
 @provider(IVocabularyFactory)
 def addon_state_vocabulary(context):
     return SimpleVocabulary([
-        SimpleTerm(value=u'alpha',
-                   title=_(u'Alpha status (prototype state: never use in production)')),  # NOQA: E501
-        SimpleTerm(value=u'beta',
-                   title=_(u'Beta status (development state: release being prepared)')),  # NOQA: E501
-        SimpleTerm(value=u'stable',
-                   title=_(u'Production/Stable')),
-        SimpleTerm(value=u'unmaintained',
-                   title=_(u'Unmaintained/Inactive ()')),
+        SimpleTerm(value=u'alpha', title=_(u'Alpha status (prototype state: never use in production)')),
+        SimpleTerm(value=u'beta', title=_(u'Beta status (development state: release being prepared)')),
+        SimpleTerm(value=u'stable', title=_(u'Production/Stable')),
+        SimpleTerm(value=u'unmaintained', title=_(u'Unmaintained/Inactive ()')),
     ])
