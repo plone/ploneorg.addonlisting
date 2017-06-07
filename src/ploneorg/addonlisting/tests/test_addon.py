b53de03bf14c83ab6cb78ba353bc783c98c12c87
pavithirakc <pavithira.kc@iiitb.org>
2017-06-07 20:58:37 +0530

    update test_addon

--- src/ploneorg/addonlisting/tests/test_addon.py
+++ src/ploneorg/addonlisting/tests/test_addon.py
@@ -1,10 +1,12 @@
-from pkg_resources import resource_stream
-from plone.app.testing import TEST_USER_ID
+from plone import api
 from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
 from plone.dexterity.interfaces import IDexterityFTI
-from ploneorg.addonlisting.testing import PLONEORG_ADDONLISTING_INTEGRATION_TESTING
+from ploneorg.addonlisting.interfaces import IAddOn
+from ploneorg.addonlisting.testing import PLONEORG_ADDONLISTING_INTEGRATION_TESTING  # noqa
 from zope.component import createObject
 from zope.component import queryUtility
+
 import unittest
 
 
@@ -13,25 +15,28 @@ class AddOnIntegrationTest(unittest.TestCase):
     layer = PLONEORG_ADDONLISTING_INTEGRATION_TESTING
 
     def setUp(self):
+        """Custom shared utility setup for tests."""
         self.portal = self.layer['portal']
         setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.installer = api.portal.get_tool('portal_quickinstaller')
+
+    def test_schema(self):
+        fti = queryUtility(IDexterityFTI, name='AddOn')
+        schema = fti.lookupSchema()
+        self.assertEqual(schema, schema)
 
     def test_fti(self):
         fti = queryUtility(IDexterityFTI, name='AddOn')
         self.assertTrue(fti)
-    
-    def test_schema(self):
-        fti = queryUtility(IDexterityFTI, name='AddOn')
-        schema = fti.lookupSchema()
-        self.assertTrue(schema)
-        
+
     def test_factory(self):
         fti = queryUtility(IDexterityFTI, name='AddOn')
         factory = fti.factory
-        AddOn = createObject(factory)
-        self.assertTrue(AddOn)
+        obj = createObject(factory)
+        self.assertTrue(IAddOn.providedBy(obj))
 
     def test_adding(self):
         self.portal.invokeFactory('AddOn', 'AddOn')
-        self.assertTrue(self.portal.AddOn)
-        
+        self.assertTrue(
+            IAddOn.providedBy(self.portal['AddOn'])
+        )
