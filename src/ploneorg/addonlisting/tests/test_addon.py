c7d30a9017a829e7c9ec0d839431811fab5eead9
Alexander Loechel <Alexander.Loechel@lmu.de>
2017-06-08 21:57:24 +0200

    fix tests and examples

--- src/ploneorg/addonlisting/tests/test_addon.py
+++ src/ploneorg/addonlisting/tests/test_addon.py
@@ -1,8 +1,10 @@
 from plone import api
+from plone.api.exc import InvalidParameterError
 from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID
 from plone.dexterity.interfaces import IDexterityFTI
 from ploneorg.addonlisting.interfaces import IAddOn
+from ploneorg.addonlisting.interfaces import IAddOnFolder
 from ploneorg.addonlisting.testing import PLONEORG_ADDONLISTING_INTEGRATION_TESTING  # noqa
 from zope.component import createObject
 from zope.component import queryUtility
@@ -23,6 +25,7 @@ class AddOnIntegrationTest(unittest.TestCase):
     def test_schema(self):
         fti = queryUtility(IDexterityFTI, name='AddOn')
         schema = fti.lookupSchema()
+        # self.assertEqual(IAddOnFolder, schema)  # TODO: fix test
         self.assertEqual(schema, schema)
 
     def test_fti(self):
@@ -35,8 +38,37 @@ class AddOnIntegrationTest(unittest.TestCase):
         obj = createObject(factory)
         self.assertTrue(IAddOn.providedBy(obj))
 
-    def test_adding(self):
-        self.portal.invokeFactory('AddOn', 'AddOn')
+    def test_adding_globaly(self):
+        portal = api.portal.get()
+        with self.assertRaises(ValueError,
+                               message='Disallowed subobject type: AddOn'):
+            self.portal.invokeFactory('AddOn', 'AddOn')
+
+        with self.assertRaisesRegexp(InvalidParameterError,
+                                     "Cannot add a 'AddOn' "
+                                     "object to the container."):
+            addon = api.content.create(  # NOQA: F841
+                type='AddOn',
+                title='Pillow',
+                container=portal
+            )
+
+    def test_adding_in_addonfolder(self):
+        portal = api.portal.get()
+        addon_folder = api.content.create(
+            type='AddOnFolder',
+            title='Plone',
+            container=portal
+        )
+
+        addon = api.content.create(
+            type='AddOn',
+            title='Pillow',
+            container=addon_folder
+        )
         self.assertTrue(
-            IAddOn.providedBy(self.portal['AddOn'])
+            IAddOnFolder.providedBy(addon_folder)
+        )
+        self.assertTrue(
+            IAddOn.providedBy(addon)
         )
