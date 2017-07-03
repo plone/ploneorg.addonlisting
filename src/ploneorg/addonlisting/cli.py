86114539f713c5b9bd439f53a0b70c8cb71e47e9
pavithirakc <pavithira.kc@iiitb.org>
2017-07-03 19:17:41 +0530

    add,parse args

--- src/ploneorg/addonlisting/cli.py
+++ src/ploneorg/addonlisting/cli.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 
-# http://pastebin.com/3u1gNCD3
+from ploneorg.addonlisting.utils import update_addon
+from ploneorg.addonlisting.utils import update_addons
+from ploneorg.addonlisting.utils import update_addon_list
+from zope.site.hooks import setSite
 
-# import argparse
+import argparse
 import logging
 
 
@@ -11,18 +14,90 @@ log = logging.getLogger('ploneorg.addonlisting-cli')
 
 
 def update_addon_listing():
-    # Parse the command line arguments
-    # argparser = argparse.ArgumentParser()
-    pass
+    parser = argparse.ArgumentParser(
+        description="updates the listing with any new addons"
+    )
+    parser.add_argument(
+        "context", 
+        help="Path to AddOnFolder relative to Zope root. Ex:/Plone/<AddOnFolder name>"
+    )
+    parser.add_argument(
+        "-v", 
+        "--verbose", 
+        default=False,
+        help="print more verbose output", 
+        action="store_true"
+    )
+    parser.add_argument(
+        "--limit", 
+        type=int, 
+        default=0,
+        help="limit the number of new addons fetched from PyPI"
+    )
+    parser.add_argument(
+        "-s", 
+        "--site-id", 
+        dest='site_id', 
+        default='Plone', 
+        help="ID of the Plone site to fetch the content objects from"
+    )
+    args = parser.parse_args()
+    '''
+    current = app  # noqa
+    for part in site_id.split('/'):
+        current = current[part]
+    portal = current
+    setSite(portal)
+    print portal
 
+    '''
+    # obtain the portal root object somehow and 
+    # store in a local variable "portal"
 
+    context = portal.restrictedTraverse(args.context)
+    update_addon_list(context, verbose=args.verbose, limit=args.limit)
+    
 def update_addons():
-    # Parse the command line arguments
-    # argparser = argparse.ArgumentParser()
-    pass
-
-
+    parser = argparse.ArgumentParser(
+        description="updates all the addons in the listing"
+    )
+    parser.add_argument(
+        "context",
+        help="Path to AddOnFolder relative to Zope root. Ex:/Plone/<AddOnFolder name>"
+    )
+    parser.add_argument(
+        "-v", 
+        "--verbose", 
+        default=False,
+        help="print more verbose output", 
+        action="store_true"
+    )
+    parser.add_argument(
+        "--limit", 
+        type=int, 
+        default=0,
+        help="limit the number of addons checked for updates from PyPI"
+    )
+    args = parser.parse_args()
+    context = portal.restrictedTraverse(args.context)
+    update_addons(context, verbose=args.verbose, limit=args.limit)
+    
 def update_addon():
-    # Parse the command line arguments
-    # argparser = argparse.ArgumentParser()
-    pass
+    parser = argparse.ArgumentParser(
+        description="updates an individual addon"
+    )
+    parser.add_argument(
+        "context",
+        help="Path to AddOn relative to Zope root.  Ex:/Plone/<AddOnFolder name>/<AddOn name>"
+    )
+    parser.add_argument(
+        "-v", 
+        "--verbose",
+        default=False,
+        help="print more verbose output", 
+        action="store_true"
+    )
+    args = parser.parse_args()
+    context = portal.restrictedTraverse(args.context)
+    update_addon(context, verbose=args.verbose)
+    
